9bc8f29ddee65f020f7fcc2fcda4d6c4c7ec3bfc
Goodman <dgoodman@ic.ac.uk>
2020-09-16 18:00:16 +0100

    improve timezone dist and dump output to file

--- test_scheduling.py
+++ test_scheduling.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy import stats
 from random import shuffle
 import matplotlib.pyplot as plt
+import pickle
+import os
 
 #%% Parameters
 # Participants
@@ -15,7 +17,7 @@ num_talks = 1_000
 num_days = 5
 num_times = num_days*24
 # (p, start_time, end_time)
-timezones = [(0.25, 0, 10), (0.25, 4, 14), (0.25, 8, 18), (0.25, 0, 24)]
+timezones = [(0.25, 0, 10), (0.25, 4, 14), (0.25, 8, 18), (0.25, 14, 28)]
 mean_available_per_day = stats.uniform(0, 10)
 available_per_day = lambda mu: stats.binom(10., mu/10.)
 
@@ -34,12 +36,12 @@ for i in range(num_participants):
     _, start, end = timezones[participant_timezones[i]]
     mu = mean_available_per_day.rvs(size=1)
     n_avail = available_per_day(mu).rvs(size=num_days)
-    I = np.arange(start, end)
+    I = np.arange(start, end) % 24
     free = []
     for day in range(num_days):
         shuffle(I)
         free.extend(day*24+I[:n_avail[day]])
-    free_times.append(np.array(free))
+    free_times.append(np.array(free, dtype=int))
 
 #%% Generate talks and preferences
 participant_clusters = stats.randint(0, clusters).rvs(size=num_participants)
@@ -53,8 +55,21 @@ for i in range(num_participants):
     preferred = (stats.uniform.rvs(size=num_talks)<p).nonzero()
     prefs.append(preferred)
 
-#%% Quickly get stats on how many are interested in each talk
+#%% Quickly generate some plots/stats
 interested_count = np.zeros(num_talks, dtype=int)
 for p in prefs:
     interested_count[p] += 1
-#plt.hist(interested_count, bins=20)
+plt.figure()
+plt.hist(interested_count, bins=20)
+
+available_count = np.zeros(num_times, dtype=int)
+for free in free_times:
+    available_count[free] += 1
+plt.figure()
+plt.bar(np.arange(num_times), available_count, width=1.0)
+plt.xlim(0, 24)
+#plt.savefig('free_times.png')
+
+#%% Dump data to pickle file
+data = {'free_times': free_times, 'prefs': prefs}
+pickle.dump(data, open('times_and_prefs.pickle', 'wb'))
